5a8b35363f2abe7439637c7ef9565409308a0ec4
haoxiaopang <zipu888@sina.com>
2013-11-23 19:57:08 +0800

    add a table

--- dbscript/db.sql
+++ dbscript/db.sql
@@ -1,1 +1,14 @@
-----create table scirpt------
+
+drop table if exists adminUser;
+
+/*==============================================================*/
+/* Table: adminUser                                             */
+/*==============================================================*/
+create table adminUser
+(
+   id                   bigint not null auto_increment,
+   realName             varchar(500),
+   loginName            varchar(500),
+   password             varchar(500),
+   primary key (id)
+);
