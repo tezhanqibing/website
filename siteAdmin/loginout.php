f2ffcdd8ec8a8e6d7f1d9004b77676dd066d2339
haoxiaopang <zipu888@sina.com>
2013-11-24 14:48:36 +0800

    modify

---
+++ siteAdmin/loginout.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: xiqohao
+ * Date: 13-11-24
+ * Time: 下午2:47
+ * To change this template use File | Settings | File Templates.
+ */
+    echo "您已经成功退出!";
+?>
