376a2221b4085c1f1e45b8ae9d1fc98bbcf02fb4
haoxiaopang <zipu888@sina.com>
2013-12-14 21:29:23 +0800

    commit cache and log

---
+++ app/Runtime/Cache/4dfb9a103d3dbabc20cce71c594dc4bb.php
@@ -0,0 +1,36 @@
+<?php if (!defined('THINK_PATH')) exit();?><link href="__PUBLIC__/images/skin.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style type="text/css">
+    <!--
+    body {
+        margin-left: 0px;
+        margin-top: 0px;
+        margin-right: 0px;
+        margin-bottom: 0px;
+        background-color: #EEF2FB;
+    }
+    -->
+</style>
+<body>
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+<tr>
+    <td width="17" valign="top" background="__PUBLIC__/images/mail_leftbg.gif"><img src="__PUBLIC__/images/left-top-right.gif" width="17" height="29" /></td>
+    <td valign="top" background="__PUBLIC__/images/content-bg.gif"><table width="100%" height="31" border="0" cellpadding="0" cellspacing="0" class="left_topbg" id="table2">
+        <tr>
+            <td height="31"><div class="titlebt">公司新闻维护</div></td>
+        </tr>
+    </table></td>
+    <td width="16" valign="top" background="__PUBLIC__/images/mail_rightbg.gif"><img src="__PUBLIC__/images/nav-right-bg.gif" width="16" height="29" /></td>
+</tr>
+<tr>
+<td valign="middle" background="__PUBLIC__/images/mail_leftbg.gif">&nbsp;</td>
+<td valign="top" bgcolor="#F7F8F9"></td>
+<td background="__PUBLIC__/images/mail_rightbg.gif">&nbsp;</td>
+</tr>
+<tr>
+    <td valign="bottom" background="__PUBLIC__/images/mail_leftbg.gif"><img src="__PUBLIC__/images/buttom_left2.gif" width="17" height="17" /></td>
+    <td background="__PUBLIC__/images/buttom_bgs.gif"><img src="__PUBLIC__/images/buttom_bgs.gif" width="17" height="17"></td>
+    <td valign="bottom" background="__PUBLIC__/images/mail_rightbg.gif"><img src="__PUBLIC__/images/buttom_right2.gif" width="16" height="17" /></td>
+</tr>
+</table>
+</body>
