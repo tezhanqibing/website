25cdb91b7c3726fcc963aebd05363dd2fbbba8fc
haoxiaopang <zipu888@sina.com>
2013-12-14 15:42:18 +0800

    change db password

--- app/Conf/config.php
+++ app/Conf/config.php
@@ -5,7 +5,7 @@ return array(
         'DB_HOST'   => 'localhost', // 服务器地址
         'DB_NAME'   => 'test', // 数据库名
         'DB_USER'   => 'root', // 用户名
-        'DB_PWD'    => 'root', // 密码
+        'DB_PWD'    => 'xiaohao', // 密码
         'DB_PORT'   => 3306, // 端口
         'DB_PREFIX' => '', // 数据库表前缀 
         //其他项目配置参数
